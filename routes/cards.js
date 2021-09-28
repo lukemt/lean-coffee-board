8cca07f14f86633eff5d8cf1888a1b1325669c9e
Lukas Petry <lukemt@rennwurst.de>
2021-09-28 12:42:37 +0200

    add local memory store

--- routes/cards.js
+++ routes/cards.js
@@ -1,28 +1,63 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/api/cards/:id', (req, res) => {
+let cards = [
+  {
+    id: 0,
+    name: 'Template Card',
+    description: 'This is a template card',
+    image: 'https://picsum.photos/200/300/?random',
+    likes: 0,
+  },
+  {
+    id: 1,
+    name: 'Card 1',
+    description: 'This is card 1',
+    image: 'https://picsum.photos/200/300/?random',
+    likes: 0,
+  },
+  {
+    id: 2,
+    name: 'Card 2',
+    description: 'This is card 2',
+    image: 'https://picsum.photos/200/300/?random',
+    likes: 0,
+  },
+]
+
+router.get('/', (req, res) => {
+  res.status(205)
+  res.send(cards)
+})
+
+router.get('/:id', (req, res) => {
   const id = req.params.id
-  res.send(`Card with id ${id}`)
+  res.send(cards.find(card => card.id == id))
 })
 
-router.post('/api/cards/', (req, res) => {
-  const card = req.body
-  res.send(card)
+router.post('/', (req, res) => {
+  const newCard = { ...cards[0], ...req.body, id: cards.at(-1).id + 1 }
+  res.send(newCard)
 })
 
-router.put('/api/cards/:id', (req, res) => {
-  const answer = { ...req.params, ...req.body }
-  console.log(req.body)
-  res.send(answer)
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  const newCard = { ...cards[0], ...req.body, id }
+  cards = cards.map(card => (card.id == id ? newCard : card))
+  res.send(newCard)
 })
 
-router.patch('/api/cards/:id', (req, res) => {
-  const answer = { ...req.params, ...req.body }
-  console.log(req.body)
-  res.send(answer)
+router.patch('/:id', (req, res) => {
+  const id = req.params.id
+  cards = cards.map(card => (card.id == id ? { ...card, ...req.body } : card))
+  res.send(cards.find(card => card.id == id))
 })
 
-router.delete('/api/cards/:id', (req, res) => {})
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
+  const deletedCard = cards.find(card => card.id == id)
+  cards = cards.filter(card => card.id != id)
+  res.send(deletedCard)
+})
 
 module.exports = router
