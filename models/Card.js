f9c72b7988023125291b07a6f436483697dd2ce0
Lukas Petry <lukemt@rennwurst.de>
2021-10-04 10:39:55 +0200

    add comment

--- models/Card.js
+++ models/Card.js
@@ -15,6 +15,8 @@ schema.virtual('everything').get(function () {
   return `${this.text} ${this.author}`
 })
 
+// Collection: 'cards'
+// Mongoose Model: 'Card' --> MongoDB Collection: 'cards'
 const model = mongoose.model('Card', schema)
 
 module.exports = model
